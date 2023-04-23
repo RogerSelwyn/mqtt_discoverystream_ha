bab801d8e988f7e59e32070acc16379bbd642856
RogerSelwyn <rogers@selwyn.org.uk>
2023-04-23 11:13:05 +0100

    Revert change

--- custom_components/mqtt_discoverystream/__init__.py
+++ custom_components/mqtt_discoverystream/__init__.py
@@ -26,7 +26,14 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     # sourcery skip: assign-if-exp, boolean-if-exp-identity, reintroduce-else
     """Set up the MQTT state feed."""
-
+    # Make sure MQTT is available and the entry is loaded
+    if not hass.config_entries.async_entries(
+        mqtt.DOMAIN
+    ) or not await hass.config_entries.async_wait_component(
+        hass.config_entries.async_entries(mqtt.DOMAIN)[0]
+    ):
+        _LOGGER.error("MQTT integration is not available")
+        return False
     conf: ConfigType = config[DOMAIN]
     publish_filter = convert_include_exclude_filter(conf)
     base_topic: str = conf.get(CONF_BASE_TOPIC)
