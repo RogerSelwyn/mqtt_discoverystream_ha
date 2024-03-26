e1c7334b9979bc375a848e7c92e79a871fe8fce7
RogerSelwyn <rogers@selwyn.org.uk>
2024-03-26 15:55:16 +0000

    feat: Mark entities unavailable at HA shutdown

--- custom_components/mqtt_discoverystream/publisher.py
+++ custom_components/mqtt_discoverystream/publisher.py
@@ -13,6 +13,7 @@ from homeassistant.components.mqtt.const import (
 from homeassistant.const import (
     CONF_INCLUDE,
     EVENT_HOMEASSISTANT_STARTED,
+    EVENT_HOMEASSISTANT_STOP,
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
     Platform,
@@ -74,9 +75,11 @@ class Publisher:
         self._cover = Cover(hass, self._publish_retain)
         self._discovery = Discovery(hass, conf)
         self._publish_filter = convert_include_exclude_filter(conf)
+        self._entity_states = {}
         async_when_setup(hass, MQTT_DOMAIN, self._async_subscribe)
         self._register_services()
         self._listen_for_hass_started()
+        self._listen_for_hass_stop()
 
     async def async_state_publish(self, entity_id, new_state, mybase):
         """Publish state for MQTT Discovery Statestream."""
@@ -145,9 +148,20 @@ class Publisher:
             DOMAIN, "publish_discovery_state", self._async_publish_discovery_state
         )
 
+    def _listen_for_hass_started(self):
+        self._hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_STARTED, self._async_publish_discovery_state
+        )
+        async_call_later(self._hass, self._loop_time, self._async_schedule_publish)
+
+    def _listen_for_hass_stop(self):
+        self._hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_STOP, self._async_mark_unavailable
+        )
+
     async def _async_publish_discovery_state(self, call=None):  # pylint: disable=unused-argument
         ent_reg = entity_registry.async_get(self._hass)
-        entity_states = {}
+        self._entity_states = {}
         for entity_id in list(ent_reg.entities):
             if self._publish_filter(entity_id):
                 if current_state := self._hass.states.get(entity_id):
@@ -155,19 +169,26 @@ class Publisher:
                     await self._discovery.async_discovery_publish(
                         entity_id, current_state.attributes, mybase
                     )
-                    entity_states[entity_id] = current_state
-        _LOGGER.info("Discovery published")
+                    self._entity_states[entity_id] = current_state
+        _LOGGER.debug("Discovery published")
         await asyncio.sleep(DEFAULT_STATE_SLEEP)
-        for entity_id, current_state in entity_states.items():
+        for entity_id, current_state in self._entity_states.items():
             mybase = f"{self._base_topic}{entity_id.replace('.', '/')}/"
             await self.async_state_publish(entity_id, current_state, mybase)
-        _LOGGER.info("States published")
+        _LOGGER.debug("States published")
 
-    def _listen_for_hass_started(self):
-        self._hass.bus.async_listen_once(
-            EVENT_HOMEASSISTANT_STARTED, self._async_publish_discovery_state
-        )
-        async_call_later(self._hass, self._loop_time, self._async_schedule_publish)
+    async def _async_mark_unavailable(self, call):  # pylint: disable=unused-argument
+        _LOGGER.info("Shutdown - marking entities unavailable")
+        for entity_id in self._entity_states:
+            mybase = f"{self._base_topic}{entity_id.replace('.', '/')}/"
+            _LOGGER.info(entity_id)
+            await mqtt.async_publish(
+                self._hass,
+                f"{mybase}{CONF_AVAILABILITY}",
+                DEFAULT_PAYLOAD_NOT_AVAILABLE,
+                0,
+                self._publish_retain,
+            )
 
     async def _async_schedule_publish(self, recalltime):  # pylint: disable=unused-argument
         await self._async_publish_discovery_state()
